ec131d7af5563a3836168e2879d8c76c9a768469
VKV-Vyvoj <144177437+VKV-Vyvoj@users.noreply.github.com>
2023-09-06 01:12:02 +0200

    Update

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -5,7 +5,6 @@
     import { get, ref, set } from "firebase/database";
     import { getDownloadURL, ref as storageRef, uploadBytes } from "firebase/storage";
     import Swal from "sweetalert2";
-  
     import type { User } from "firebase/auth";
   
     let user: User | null = null;
@@ -20,6 +19,9 @@
     let interests: string[] = [];
     let orientation: string = "";
     let lookingFor: string[] = [];
+  
+    let step: number = 1; // Krok 1: Vyplňování informací, Krok 2: Nahrávání fotek, Krok 3: Zobrazení účtu
+  
     let profileImage: File | null = null;
     let additionalImages: File[] = [];
   
@@ -71,60 +73,27 @@
       }
     });
   
-    let step = 1; // Krok 1 pro vyplňování informací, krok 2 pro nahrávání fotek
-  
-    const uploadProfileImage = async () => {
-      if (user && profileImage) {
-        try {
-          const storageRef = ref(storage, `users/${user.uid}/profile-image.jpg`);
-          await uploadBytes(storageRef, profileImage);
-          const downloadURL = await getDownloadURL(storageRef);
-          return downloadURL;
-        } catch (error) {
-          console.error("Chyba při nahrávání profilové fotky:", error);
-          return null;
-        }
-      }
-      return null;
-    };
-  
-    const uploadAdditionalImages = async () => {
-      if (user && additionalImages.length > 0) {
-        try {
-          const imagePromises = additionalImages.map(async (image, index) => {
-            const storageRef = ref(storage, `users/${user.uid}/image${index + 1}.jpg`);
-            await uploadBytes(storageRef, image);
-            return getDownloadURL(storageRef);
-          });
-          const downloadURLs = await Promise.all(imagePromises);
-          return downloadURLs;
-        } catch (error) {
-          console.error("Chyba při nahrávání dalších fotek:", error);
-          return [];
-        }
-      }
-      return [];
-    };
-  
     const saveData = async () => {
       if (step === 1) {
-        // Pokud jsme na kroku 1, uložíme informace
-        const userRef = ref(db, `users/${user!.uid}`);
-        set(userRef, {
-          displayName,
-          age,
-          country,
-          city,
-          bio,
-          interests,
-          orientation,
-          lookingFor,
-          isSurveyCompleted: true,
-        }).then(() => {
-          step = 2; // Přechod na krok 2 pro nahrávání fotek
-        });
+        if (!displayName || !age || !country || !city || !bio || interests.length === 0 || !orientation || lookingFor.length === 0) {
+          Swal.fire({
+            icon: "error",
+            title: "Všechna pole jsou povinná",
+            text: "Vyplňte všechna pole před pokračováním.",
+          });
+          return;
+        }
+        step = 2;
       } else if (step === 2) {
-        // Pokud jsme na kroku 2, uložíme fotky a přejdeme do účtu
+        if (!profileImage || additionalImages.length < 2) {
+          Swal.fire({
+            icon: "error",
+            title: "Nahrání fotek",
+            text: "Nahrát profilovou fotku a alespoň další dvě fotky je povinné.",
+          });
+          return;
+        }
+        // Nahrání fotek a uložení URL do databáze
         const profileImageUrl = await uploadProfileImage();
         const additionalImageUrls = await uploadAdditionalImages();
   
@@ -133,7 +102,7 @@
           profileImageUrl,
           additionalImageUrls,
         }).then(() => {
-          step = 3; // Přechod do účtu
+          step = 3;
           Swal.fire({
             icon: "success",
             title: "Dotazník byl úspěšně uložen!",
@@ -144,37 +113,72 @@
       }
     };
   
+    const uploadProfileImage = async () => {
+      if (!profileImage || !user) return "";
+      const storageRefPath = `profile_images/${user.uid}/${profileImage.name}`;
+      const storageRefObj = storageRef(storage, storageRefPath);
+  
+      try {
+        await uploadBytes(storageRefObj, profileImage);
+        const downloadURL = await getDownloadURL(storageRefObj);
+        return downloadURL;
+      } catch (error) {
+        console.error("Chyba při nahrávání profilové fotky:", error);
+        return "";
+      }
+    };
+  
+    const uploadAdditionalImages = async () => {
+      if (additionalImages.length === 0 || !user) return [];
+  
+      const imageUrls = [];
+      for (const image of additionalImages) {
+        const storageRefPath = `additional_images/${user.uid}/${image.name}`;
+        const storageRefObj = storageRef(storage, storageRefPath);
+  
+        try {
+          await uploadBytes(storageRefObj, image);
+          const downloadURL = await getDownloadURL(storageRefObj);
+          imageUrls.push(downloadURL);
+        } catch (error) {
+          console.error("Chyba při nahrávání dalších fotek:", error);
+        }
+      }
+  
+      return imageUrls;
+    };
+  
     const logout = () => {
-      // Odhlášení uživatele zde
+      // Odhlášení uživatele zde (signOut)
     };
   </script>
   
   <div class="h-screen w-full">
     <div class="bg-gray-800 h-screen mx-auto max-w-md">
-      {#if step === 1}
-      <!-- Karta pro vyplnění informací -->
+      {#if isFirstLogin && step === 1}
+      <!-- Karta pro vyplnění dotazníku: Krok 1 -->
       <div class="p-6 md:p-12">
         <p class="text-center text-3xl md:text-5xl pt-6 pb-3 text-yellow-500 font-bold">
           Vítejte, {displayName}!
         </p>
         <p class="text-center text-lg md:text-xl py-2 text-gray-400 font-semibold">Vyplňte dotazník</p>
         <div class="p-3 md:p-6">
-          <input placeholder="Vaše jméno" bind:value={displayName} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3" type="text" />
-          <input placeholder="Váš věk" bind:value={age} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3" type="number" />
-          <input placeholder="Vaše země" bind:value={country} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3" type="text" />
-          <input placeholder="Město" bind:value={city} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3" type="text" />
-          <textarea placeholder="Bio" bind:value={bio} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3"></textarea>
+          <input placeholder="Vaše jméno" bind:value={displayName} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3" type="text" required />
+          <input placeholder="Váš věk" bind:value={age} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3" type="number" required />
+          <input placeholder="Vaše země" bind:value={country} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3" type="text" required />
+          <input placeholder="Město" bind:value={city} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3" type="text" required />
+          <textarea placeholder="Bio" bind:value={bio} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3" required></textarea>
           <label class="text-yellow-500 mt-3 mb-2">Zájmy:</label>
           <div class="flex flex-wrap gap-1">
             {#each availableInterests as interest}
             <div class="bg-gray-700 p-1 rounded-lg flex items-center gap-2">
-              <input type="checkbox" id={interest} bind:group={interests} value={interest} class="form-checkbox h-4 w-4 text-yellow-500" />
+              <input type="checkbox" id={interest} bind:group={interests} value={interest} class="form-checkbox h-4 w-4 text-yellow-500" required />
               <label for={interest} class="text-white text-xs">{interest}</label>
             </div>
             {/each}
           </div>
           <label class="text-yellow-500 mt-3 mb-2">Orientace:</label>
-          <select bind:value={orientation} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3">
+          <select bind:value={orientation} class="bg-transparent text-yellow-500 w-full focus:outline-none focus:ring mb-3" required>
             {#each availableOrientations as option}
             <option value={option}>{option}</option>
             {/each}
@@ -183,32 +187,35 @@
           <div class="flex flex-wrap gap-1">
             {#each availableLookingFor as item}
             <div class="bg-gray-700 p-1 rounded-lg flex items-center gap-2">
-              <input type="checkbox" id={item} bind:group={lookingFor} value={item} class="form-checkbox h-4 w-4 text-yellow-500" />
+              <input type="checkbox" id={item} bind:group={lookingFor} value={item} class="form-checkbox h-4 w-4 text-yellow-500" required />
               <label for={item} class="text-white text-xs">{item}</label>
             </div>
             {/each}
           </div>
+          <label class="text-yellow-500 mt-3 mb-2">Profilová fotka (povinná):</label>
+          <input type="file" accept="image/*" on:change={(e) => (profileImage = e.target.files[0])} required />
+          <label class="text-yellow-500 mt-3 mb-2">Další fotografie (minimálně 2, povinné):</label>
+          <input type="file" accept="image/*" multiple on:change={(e) => (additionalImages = [...e.target.files])} required />
           <button on:click={saveData} class="bg-yellow-500 p-3 rounded-3xl w-full h-full hover:bg-yellow-600 mt-4">Pokračovat</button>
         </div>
       </div>
-      {/if}
-      {#if step === 2}
-      <!-- Karta pro nahrávání fotek -->
+      {:else if step === 2}
+      <!-- Krok 2: Nahrávání fotek -->
       <div class="p-6 md:p-12">
         <p class="text-center text-3xl md:text-5xl pt-6 pb-3 text-yellow-500 font-bold">
-          Nahrát fotky
+          Nahrávání fotek
         </p>
+        <p class="text-center text-lg md:text-xl py-2 text-gray-400 font-semibold">Nahrát profilovou fotku a alespoň další dvě fotky.</p>
         <div class="p-3 md:p-6">
-          <label class="text-yellow-500 mt-3 mb-2">Profilová fotka:</label>
-          <input type="file" accept="image/*" on:change={(e) => (profileImage = e.target.files[0])} />
-          <label class="text-yellow-500 mt-3 mb-2">Další fotografie (max. 10):</label>
-          <input type="file" accept="image/*" multiple on:change={(e) => (additionalImages = [...e.target.files])} />
+          <label class="text-yellow-500 mt-3 mb-2">Profilová fotka (povinná):</label>
+          <input type="file" accept="image/*" on:change={(e) => (profileImage = e.target.files[0])} required />
+          <label class="text-yellow-500 mt-3 mb-2">Další fotografie (minimálně 2, povinné):</label>
+          <input type="file" accept="image/*" multiple on:change={(e) => (additionalImages = [...e.target.files])} required />
           <button on:click={saveData} class="bg-yellow-500 p-3 rounded-3xl w-full h-full hover:bg-yellow-600 mt-4">Uložit dotazník</button>
         </div>
       </div>
-      {/if}
-      {#if step === 3}
-      <!-- Přechod do účtu -->
+      {:else}
+      <!-- Krok 3: Zobrazení účtu -->
       <div class="p-6 md:p-12">
         <p class="text-center text-3xl md:text-5xl pt-6 pb-3 text-yellow-500 font-bold">
           Vítejte, {displayName}!
