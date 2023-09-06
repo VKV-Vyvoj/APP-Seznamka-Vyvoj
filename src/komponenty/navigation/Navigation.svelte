969de2e8f6d54fc68afed4aa09fd0899cdd393d8
VKV-Vyvoj <144177437+VKV-Vyvoj@users.noreply.github.com>
2023-09-06 02:31:55 +0200

    test

---
+++ src/komponenty/navigation/Navigation.svelte
@@ -0,0 +1,28 @@
+ <!-- Navigační menu -->
+ <div class="fixed bottom-0 left-0 right-0 bg-gray-800 flex justify-between p-4">
+    <!-- Tlačítko Domů -->
+    <button class="text-white flex flex-col items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mb-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V3z"/>
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 10a7 7 0 0 1-7 7"/>
+      </svg>
+      Home
+    </button>
+    
+    <!-- Tlačítko Zprávy -->
+    <button class="text-white flex flex-col items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mb-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"/>
+      </svg>
+      Zprávy
+    </button>
+    
+    <!-- Tlačítko Lidé -->
+    <button class="text-white flex flex-col items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mb-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"/>
+      </svg>
+      Lidé
+    </button>
+    </div>
+    
